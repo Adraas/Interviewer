06edbb5a4b5a7986c5cead10a8c935a85b7278b4
Adraas <pickalov.artyom@yandex.ru>
2019-06-07 20:22:34 +0400

    docs: add missing documentation

--- repository/src/main/java/ru/wkn/entities/questionnaire/Answer.java
+++ repository/src/main/java/ru/wkn/entities/questionnaire/Answer.java
@@ -1,8 +1,24 @@
 package ru.wkn.entities.questionnaire;
 
+/**
+ * The class {@code Answer} represents single answer for questionnaire's question.
+ *
+ * @author Orin Adraas
+ */
 public class Answer {
 
+    /**
+     * The unique ID of answer.
+     */
     private long id;
+
+    /**
+     * The ID of question, to whom answer belongs.
+     */
     private long questionId;
+
+    /**
+     * The answer wording.
+     */
     private String answerWording;
 }
