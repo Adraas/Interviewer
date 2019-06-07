64400d5e2def3774c75b5c2131efe013cf230c9b
Adraas <pickalov.artyom@yandex.ru>
2019-06-07 21:38:09 +0400

    feature: start ORM for entities

--- repository/src/main/java/ru/wkn/entities/questionnaire/Question.java
+++ repository/src/main/java/ru/wkn/entities/questionnaire/Question.java
@@ -1,26 +1,45 @@
 package ru.wkn.entities.questionnaire;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
 import java.util.Set;
 
 /**
- * The class {@code Question} represents single questionnaire's question with preset answers.
+ * The class {@code Question} represents single questionnaire's question wording with preset answers.
  *
  * @author Orin Adraas
  */
+@Entity(name = "question")
+@Table(name = "question")
 public class Question {
 
     /**
      * The unique ID of question.
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false, insertable = false, updatable = false)
     private long id;
 
     /**
      * The question wording.
      */
-    private String question;
+    @Column(name = "question_wording", unique = true, nullable = false, length = 300)
+    private String questionWording;
 
     /**
      * The question preset answers.
      */
-    private Set<String> answers;
+    @ElementCollection(targetClass = Answer.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "answer",
+            joinColumns = {@JoinColumn(name = "question_id", referencedColumnName = "answer_wording")})
+    private Set<Answer> answers;
 }
--- repository/src/main/java/ru/wkn/entities/user/User.java
+++ repository/src/main/java/ru/wkn/entities/user/User.java
@@ -1,34 +1,50 @@
 package ru.wkn.entities.user;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * The class {@code User} represents information about user of the system.
  *
  * @author Orin Adraas
  */
+@Entity(name = "user")
+@Table(name = "user")
 public class User {
 
     /**
      * The unique ID of user.
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false, insertable = false, updatable = false)
     private long id;
 
     /**
      * The pseudo-name or real name of user.
      */
+    @Column(name = "name", nullable = false, length = 60)
     private String name;
 
     /**
      * The email of user.
      */
+    @Column(name = "email", unique = true, nullable = false, length = 60)
     private String email;
 
     /**
      * The password of user account.
      */
+    @Column(name = "password", nullable = false, length = 60)
     private String password;
 
     /**
      * The cookie for the unique indicating user in the system.
      */
+    @Column(name = "cookie", unique = true, nullable = false, length = 60)
     private String cookie;
 }
