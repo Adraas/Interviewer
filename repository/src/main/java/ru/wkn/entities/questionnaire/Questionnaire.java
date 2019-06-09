46163805fa0e70aad5d5bd14eb09d52ce954ee72
Adraas <pickalov.artyom@yandex.ru>
2019-06-09 16:13:41 +0400

    feature: add ORM for entities Questionnaire and QuestionnaireGroup entities now has ORM description.

--- repository/src/main/java/ru/wkn/entities/questionnaire/Questionnaire.java
+++ repository/src/main/java/ru/wkn/entities/questionnaire/Questionnaire.java
@@ -1,5 +1,14 @@
 package ru.wkn.entities.questionnaire;
 
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import java.util.Set;
 
 /**
@@ -7,30 +16,41 @@ import java.util.Set;
  *
  * @author Orin Adraas
  */
+@Entity(name = "questionnaire")
+@Table(name = "questionnaire",
+        uniqueConstraints = {@UniqueConstraint(name = "questionnaire_unique",
+                columnNames = {"author_id", "title", "question"})})
 public class Questionnaire {
 
     /**
      * The unique ID of questionnaire.
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false, insertable = false, updatable = false)
     private long id;
 
     /**
      * The ID of questionnaire author.
      */
+    @Column(name = "author_id", nullable = false)
     private String authorId;
 
     /**
      * The title of questionnaire.
      */
+    @Column(name = "title", nullable = false, length = 100)
     private String title;
 
     /**
      * The preset questions of questionnaire.
      */
+    @ElementCollection(targetClass = Question.class, fetch = FetchType.EAGER)
     private Set<Question> questions;
 
     /**
      * The cookie for unique indicating questionnaire in the system.
      */
+    @Column(name = "cookie", unique = true, nullable = false, length = 900)
     private String cookie;
 }
--- repository/src/main/java/ru/wkn/entities/questionnaire/QuestionnaireGroup.java
+++ repository/src/main/java/ru/wkn/entities/questionnaire/QuestionnaireGroup.java
@@ -1,5 +1,14 @@
 package ru.wkn.entities.questionnaire;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.Set;
 
 /**
@@ -7,25 +16,33 @@ import java.util.Set;
  *
  * @author Orin Adraas
  */
+@Entity(name = "questionnaire_group")
+@Table(name = "questionnaire_group")
 public class QuestionnaireGroup {
 
     /**
      * The unique ID of questionnaire group.
      */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false, insertable = false, updatable = false)
     private long id;
 
     /**
      * The ID of questionnaire group author.
      */
-    private String authorId;
+    @Column(name = "author_id", nullable = false, length = 60)
+    private long authorId;
 
     /**
      * The theme of questionnaire group.
      */
+    @Column(name = "theme", nullable = false, length = 30)
     private String theme;
 
     /**
      * The preset questionnaires of questionnaire group.
      */
+    @OneToMany(targetEntity = Questionnaire.class, cascade = {CascadeType.PERSIST}, fetch = FetchType.EAGER)
     private Set<Questionnaire> questionnaires;
 }
