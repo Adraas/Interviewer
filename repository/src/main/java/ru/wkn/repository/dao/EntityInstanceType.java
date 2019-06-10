f58848bcc25de9e2edd8c1a323e630a201695a4a
Adraas <pickalov.artyom@yandex.ru>
2019-06-10 21:49:17 +0400

    feature: implement enumerates

--- repository/src/main/java/ru/wkn/repository/dao/EntityInstanceType.java
+++ repository/src/main/java/ru/wkn/repository/dao/EntityInstanceType.java
@@ -1,4 +1,20 @@
 package ru.wkn.repository.dao;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import ru.wkn.entities.questionnaire.Questionnaire;
+import ru.wkn.entities.questionnaire.QuestionnaireGroup;
+import ru.wkn.entities.user.User;
+
+import javax.persistence.Table;
+
+@AllArgsConstructor
+@Getter
 public enum EntityInstanceType {
+
+    USER(User.class.getAnnotation(Table.class).name()),
+    QUESTIONNAIRE(Questionnaire.class.getAnnotation(Table.class).name()),
+    QUESTIONNAIRE_GROUP(QuestionnaireGroup.class.getAnnotation(Table.class).name());
+
+    private String entityType;
 }
