55651eca3c0a28e0251e950c6d0139c5ad48e95b
Adraas <pickalov.artyom@yandex.ru>
2019-06-11 14:23:23 +0400

    feature: add exception into method signatures

--- repository/src/main/java/ru/wkn/repository/dao/IDao.java
+++ repository/src/main/java/ru/wkn/repository/dao/IDao.java
@@ -1,17 +1,19 @@
 package ru.wkn.repository.dao;
 
+import ru.wkn.exceptions.PersistenceException;
+
 import java.io.Serializable;
 import java.util.Collection;
 
 public interface IDao<V, I extends Serializable> {
 
-    boolean create(V newInstance);
+    boolean create(V newInstance) throws PersistenceException;
 
     V read(I index);
 
-    boolean update(V transientInstance);
+    boolean update(V transientInstance) throws PersistenceException;
 
-    boolean delete(V transientInstance);
+    boolean delete(V transientInstance) throws PersistenceException;
 
     Collection<V> getAll();
 }
