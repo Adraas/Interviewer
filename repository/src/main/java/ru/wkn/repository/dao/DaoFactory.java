1a0d2922e1f4e8a0f2863c710a4b7cde66416563
Adraas <pickalov.artyom@yandex.ru>
2019-06-11 17:58:37 +0400

    feature: implements DaoFactory

--- repository/src/main/java/ru/wkn/repository/dao/DaoFactory.java
+++ repository/src/main/java/ru/wkn/repository/dao/DaoFactory.java
@@ -1,6 +1,12 @@
 package ru.wkn.repository.dao;
 
 import org.hibernate.Session;
+import ru.wkn.entities.questionnaire.Questionnaire;
+import ru.wkn.entities.questionnaire.QuestionnaireGroup;
+import ru.wkn.entities.user.User;
+import ru.wkn.repository.dao.h2.H2QuestionnaireDao;
+import ru.wkn.repository.dao.h2.H2QuestionnaireGroupDao;
+import ru.wkn.repository.dao.h2.H2UserDao;
 
 import java.io.Serializable;
 
@@ -15,8 +21,15 @@ public class DaoFactory<V, I extends Serializable> implements IDaoFactory<V, I> 
     /**
      * @see IDaoFactory#createDao(EntityInstanceType, Session)
      */
+    @SuppressWarnings(value = {"unchecked"})
     @Override
     public IDao<V, I> createDao(EntityInstanceType entityInstanceType, Session session) {
-        return null;
+        return entityInstanceType.equals(EntityInstanceType.USER)
+                ? (IDao<V, I>) new H2UserDao(User.class, session, entityInstanceType)
+                : entityInstanceType.equals(EntityInstanceType.QUESTIONNAIRE)
+                ? (IDao<V, I>) new H2QuestionnaireDao(Questionnaire.class, session, entityInstanceType)
+                : entityInstanceType.equals(EntityInstanceType.QUESTIONNAIRE_GROUP)
+                ? (IDao<V, I>) new H2QuestionnaireGroupDao(QuestionnaireGroup.class, session, entityInstanceType)
+                : null;
     }
 }
