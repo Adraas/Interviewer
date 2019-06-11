14f2d48a0d1c85441f99bac1fd4f55460d73d37d
Adraas <pickalov.artyom@yandex.ru>
2019-06-11 14:24:17 +0400

    feature: implement DAO layout for general case

--- repository/src/main/java/ru/wkn/repository/dao/h2/H2Dao.java
+++ repository/src/main/java/ru/wkn/repository/dao/h2/H2Dao.java
@@ -1,12 +1,20 @@
 package ru.wkn.repository.dao.h2;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.java.Log;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+import ru.wkn.exceptions.PersistenceException;
 import ru.wkn.repository.dao.EntityInstanceType;
 import ru.wkn.repository.dao.IDao;
 
+import javax.persistence.Query;
+import javax.persistence.RollbackException;
 import java.io.Serializable;
 import java.util.Collection;
 
+@AllArgsConstructor
+@Log
 public class H2Dao<V, I extends Serializable> implements IDao<V, I> {
 
     private Class<V> instanceClass;
@@ -14,27 +22,59 @@ public class H2Dao<V, I extends Serializable> implements IDao<V, I> {
     private EntityInstanceType entityInstanceType;
 
     @Override
-    public boolean create(V newInstance) {
-        return false;
+    public boolean create(V newInstance) throws PersistenceException {
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.save(newInstance);
+            transaction.commit();
+        } catch (RollbackException e) {
+            transaction.rollback();
+            String message = "Element ".concat(newInstance.toString()).concat(" not created");
+            log.warning(message);
+            throw new PersistenceException(message, e);
+        }
+        return true;
     }
 
     @Override
     public V read(I index) {
-        return null;
+        return session.get(instanceClass, index);
     }
 
     @Override
-    public boolean update(V transientInstance) {
-        return false;
+    public boolean update(V transientInstance) throws PersistenceException {
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.update(transientInstance);
+            transaction.commit();
+        } catch (RollbackException e) {
+            transaction.rollback();
+            String message = "Element ".concat(transientInstance.toString()).concat(" not updated");
+            log.warning(message);
+            throw new PersistenceException(message, e);
+        }
+        return true;
     }
 
     @Override
-    public boolean delete(V transientInstance) {
-        return false;
+    public boolean delete(V transientInstance) throws PersistenceException {
+        Transaction transaction = session.beginTransaction();
+        try {
+            session.delete(transientInstance);
+            transaction.commit();
+        } catch (RollbackException e) {
+            transaction.rollback();
+            String message = "Element ".concat(transientInstance.toString()).concat(" not deleted");
+            log.warning(message);
+            throw new PersistenceException(message, e);
+        }
+        return true;
     }
 
+    @SuppressWarnings(value = {"unchecked"})
     @Override
     public Collection<V> getAll() {
-        return null;
+        Query query = session.createSQLQuery("SELECT * FROM ".concat(entityInstanceType.getEntityType()));
+        return query.getResultList();
     }
 }
