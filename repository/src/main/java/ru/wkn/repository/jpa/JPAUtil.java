aa6c84d0dceddfe5ee7e7c12bd87c0a38e7d09e9
Adraas <pickalov.artyom@yandex.ru>
2019-06-12 10:41:57 +0400

    feature: implement JPAUtil

--- repository/src/main/java/ru/wkn/repository/jpa/JPAUtil.java
+++ repository/src/main/java/ru/wkn/repository/jpa/JPAUtil.java
@@ -3,9 +3,22 @@ package ru.wkn.repository.jpa;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+/**
+ * The class {@code JPAUtil} initializes configuration for a JPA ORM model with the given persistence unit name for
+ * the {@code H2 Database} with the {@code Hibernate} provider.
+ *
+ * @author Orin Adraas
+ */
 public class JPAUtil {
 
+    /**
+     * The persistence unit name indicated a JPA configuration.
+     */
     private static final String PERSISTENCE_UNIT;
+
+    /**
+     * The {@code EntityManagerFactory} object for an {@code EntityManager} process creating ruling.
+     */
     private static final EntityManagerFactory ENTITY_MANAGER_FACTORY;
 
     static {
@@ -13,6 +26,11 @@ public class JPAUtil {
         ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
     }
 
+    /**
+     * The getter for the {@code EntityManagerFactory} object.
+     *
+     * @return the value of the {@code ENTITY_MANAGER_FACTORY} field
+     */
     public static EntityManagerFactory getEntityManagerFactory() {
         return ENTITY_MANAGER_FACTORY;
     }
