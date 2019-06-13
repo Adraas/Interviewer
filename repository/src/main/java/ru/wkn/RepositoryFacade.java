623d55445704edd7b966882c0b9235c5ca7707b4
Adraas <pickalov.artyom@yandex.ru>
2019-06-13 20:13:18 +0400

    feature: implement facade of this module

--- repository/src/main/java/ru/wkn/RepositoryFacade.java
+++ repository/src/main/java/ru/wkn/RepositoryFacade.java
@@ -1,9 +1,47 @@
 package ru.wkn;
 
+import lombok.Getter;
+import ru.wkn.repository.service.IService;
+import ru.wkn.repository.service.IServiceFactory;
+import ru.wkn.repository.service.ServiceFactory;
+import ru.wkn.repository.util.DAORepository;
+import ru.wkn.repository.util.EntityInstanceType;
+
 /**
  * The class {@code RepositoryFacade} represents Facade pattern for the repository module.
  *
  * @author Orin Adraas
  */
 public class RepositoryFacade {
+
+    @Getter
+    private String persistenceUnitName;
+    @Getter
+    private EntityInstanceType entityInstanceType;
+
+    private IServiceFactory serviceFactory;
+    @Getter
+    private IService service;
+
+    public RepositoryFacade(String persistenceUnitName, EntityInstanceType entityInstanceType) {
+        this.persistenceUnitName = persistenceUnitName;
+        this.entityInstanceType = entityInstanceType;
+        serviceFactory = new ServiceFactory();
+        serviceReinitialize();
+    }
+
+    private void serviceReinitialize() {
+        service = serviceFactory.createService(entityInstanceType,
+                DAORepository.getDao(persistenceUnitName, entityInstanceType));
+    }
+
+    public void setPersistenceUnitName(String persistenceUnitName) {
+        this.persistenceUnitName = persistenceUnitName;
+        serviceReinitialize();
+    }
+
+    public void setEntityInstanceType(EntityInstanceType entityInstanceType) {
+        this.entityInstanceType = entityInstanceType;
+        serviceReinitialize();
+    }
 }
