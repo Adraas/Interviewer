df7e80c689a9612ce1a964da0048193fb32a2ff2
Adraas <pickalov.artyom@yandex.ru>
2019-06-15 17:00:06 +0400

    feature: add facade pattern The facade pattern for the questionnaire-survey module without implementation was added.

---
+++ questionnaire-survey/src/main/java/ru/wkn/QuestionnaireSurveyFacade.java
@@ -0,0 +1,9 @@
+package ru.wkn;
+
+/**
+ * The class {@code QuestionnaireSurveyFacade} represents the facade pattern for this module.
+ *
+ * @author Orin Adraas
+ */
+public class QuestionnaireSurveyFacade {
+}
