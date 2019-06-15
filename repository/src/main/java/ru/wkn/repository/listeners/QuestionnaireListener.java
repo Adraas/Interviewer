3f9ebb3b0e8d7fe37702e6fb2b657c851ace00d9
Adraas <pickalov.artyom@yandex.ru>
2019-06-15 16:38:53 +0400

    feature: add ORM listener for DB events

---
+++ repository/src/main/java/ru/wkn/repository/listeners/QuestionnaireListener.java
@@ -0,0 +1,27 @@
+package ru.wkn.repository.listeners;
+
+import ru.wkn.entities.questionnaire.Questionnaire;
+
+/**
+ * The class {@code QuestionnaireListener} represents the listener for the {@code Questionnaire} entity.
+ *
+ * @author Orin Adraas
+ */
+public class QuestionnaireListener {
+
+    /**
+     * The method for the updating {@code Answer}'s grade criteria (the cascade deleting for a {@code GradeCriteria}
+     * enum types).
+     *
+     * @param questionnaire the listened {@code Questionnaire} entity for which the monitored event was executed
+     */
+    public void updateAnswerGradeCriteria(Questionnaire questionnaire) {
+        questionnaire.getQuestions().forEach(question ->
+                question.getAnswers().forEach((answer ->
+                        answer.getGrades().keySet().forEach(gradeCriteria -> {
+                            if (!questionnaire.getGradeCriteria().contains(gradeCriteria)) {
+                                answer.getGrades().remove(gradeCriteria);
+                            }
+                        }))));
+    }
+}
