0ae4bc3ccce12af42fd27de2524e35ae52defc7f
Adraas <pickalov.artyom@yandex.ru>
2019-06-16 13:05:03 +0400

    docs: add the missed documentation for the exception

--- interviewer-entities/src/main/java/ru/wkn/exceptions/EntityException.java
+++ interviewer-entities/src/main/java/ru/wkn/exceptions/EntityException.java
@@ -1,8 +1,17 @@
 package ru.wkn.exceptions;
 
+/**
+ * The class {@code EntityException} represents the custom exception for the indicating violations in the
+ * entities creating context.
+ *
+ * @author Orin Adraas
+ */
 public class EntityException extends Exception {
 
+    /**
+     * @see Exception#Exception(String)
+     */
     public EntityException(String message) {
-        super(message);
+        super("Entity creating exception cause: ".concat(message));
     }
 }
