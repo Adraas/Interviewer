02012edfbe283c7a4ab510ee345b6cb3968f8dfc
Adraas <pickalov.artyom@yandex.ru>
2019-06-16 16:17:14 +0400

    feature: add main properties to the facade of this module

--- questionnaire-survey/src/main/java/ru/wkn/QuestionnaireSurveyFacade.java
+++ questionnaire-survey/src/main/java/ru/wkn/QuestionnaireSurveyFacade.java
@@ -1,9 +1,24 @@
 package ru.wkn;
 
+import lombok.Getter;
+import ru.wkn.report.AnalyticalInformationGenerator;
+import ru.wkn.report.ReportCreator;
+import ru.wkn.report.StatisticsAnalyzer;
+
 /**
  * The class {@code QuestionnaireSurveyFacade} represents the facade pattern for this module.
  *
  * @author Orin Adraas
  */
+@Getter
 public class QuestionnaireSurveyFacade {
+
+    private StatisticsAnalyzer statisticsAnalyzer;
+    private AnalyticalInformationGenerator analyticalInformationGenerator;
+    private ReportCreator reportCreator;
+
+    public QuestionnaireSurveyFacade() {
+        statisticsAnalyzer = new StatisticsAnalyzer();
+        reportCreator = new ReportCreator();
+    }
 }
