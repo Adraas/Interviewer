7e877f81dd075ade87b557a6eee05c74b9b7e81f
Adraas <pickalov.artyom@yandex.ru>
2019-06-20 14:50:48 +0400

    feature: create missing servlets Missing servlets was created without an implementation.

---
+++ web-interface/src/main/java/ru/wkn/servlets/CatalogServlet.java
@@ -0,0 +1,13 @@
+package ru.wkn.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CatalogServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    }
+}
