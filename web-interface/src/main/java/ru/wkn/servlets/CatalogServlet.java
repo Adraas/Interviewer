213737987b0bdecd81955c69dcab09a3b02a5ba5
Adraas <pickalov.artyom@yandex.ru>
2019-06-30 14:20:00 +0400

    refactor(client-side): finish answer writing protocol

--- web-interface/src/main/java/ru/wkn/servlets/CatalogServlet.java
+++ web-interface/src/main/java/ru/wkn/servlets/CatalogServlet.java
@@ -7,65 +7,50 @@ import ru.wkn.repository.util.EntityInstanceType;
 import ru.wkn.util.CookieManager;
 import ru.wkn.util.ImmutablePair;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collection;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class CatalogServlet extends HttpServlet {
 
-    private static Map<String, Date> lastUpdatingDates = new HashMap<>();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         doPost(req, resp);
     }
 
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String cookie = CookieManager.getCookie(req);
         checkCookie(cookie, resp);
-        String attributeDate = "last-updating-date";
-        String lastUpdatingDateAsString = (String) req.getAttribute(attributeDate);
-        if (lastUpdatingDateAsString != null && !lastUpdatingDateAsString.trim().equals("")) {
-            updateLastUpdatingDate(cookie, new Date(Long.valueOf(lastUpdatingDateAsString)), resp, attributeDate);
-        } else {
-            updateLastUpdatingDate(cookie, new Date(System.currentTimeMillis()), resp, attributeDate);
+        String request = (String) req.getAttribute("request");
+        if (request != null) {
+            switch (request) {
+                case "update-data-table": {
+                    sendUpdatingDataTable(resp);
+                    break;
+                }
+                case "open-questionnaire": {
+                    req.getRequestDispatcher("/current_questionnaire").forward(req, resp);
+                    break;
+                }
+                default: {
+                    resp.getWriter().println();
+                }
+            }
         }
     }
 
-    private void updateLastUpdatingDate(String cookie, Date lastUpdatingDate, HttpServletResponse resp,
-                                        String attributeDate) throws IOException {
-        String attributeTable = "response-table";
-        if (lastUpdatingDates.containsKey(cookie)) {
-            if (!lastUpdatingDates.get(cookie).equals(lastUpdatingDate)) {
-                resp.getWriter().print(attributeDate
-                        .concat("=")
-                        .concat(String.valueOf(System.currentTimeMillis()))
-                        .concat("&")
-                        .concat(attributeTable)
-                        .concat("=")
-                        .concat(generateAnswerTable()));
-            } else {
-                resp.getWriter().println();
-            }
-        } else {
-            Date newUpdatingDate = new Date(System.currentTimeMillis());
-            lastUpdatingDates.put(cookie, newUpdatingDate);
-            resp.getWriter().print(attributeDate
-                    .concat("=")
-                    .concat(String.valueOf(System.currentTimeMillis()))
-                    .concat("&")
-                    .concat(attributeTable)
-                    .concat("=")
-                    .concat(generateAnswerTable()));
-        }
+    private void sendUpdatingDataTable(HttpServletResponse resp) throws IOException {
+        String attributeTable = "response-data";
+        resp.getWriter().print(attributeTable
+                .concat("=")
+                .concat(generateAnswerTable()));
     }
 
     @SuppressWarnings(value = {"unchecked"})
@@ -90,14 +75,26 @@ public class CatalogServlet extends HttpServlet {
         }
         questionnaires.removeAll(questionnairesForDeleting);
         for (Questionnaire questionnaire : questionnaires) {
-            setQuestionnaires.add(new ImmutablePair<>(questionnaire, null));
+            setQuestionnaires.add(new ImmutablePair<>(questionnaire, "-"));
         }
         return generateAnswerTable(setQuestionnaires);
     }
 
     private String generateAnswerTable(Set<ImmutablePair<Questionnaire, String>> questionnaires) {
-        String answerTable = "";
-        // TODO: create other implementation
+        String answerTable = "<table>\r\n\t<tbody>\r\n\t\t<tr>\r\n\t\t\t<th>Название анкеты</th>"
+                .concat("\r\n\t\t\t<th>Тематика (если есть)</th>\r\n\t\t</tr>");
+        for (ImmutablePair<Questionnaire, String> immutablePair : questionnaires) {
+            answerTable = answerTable
+                    .concat("\r\n\t\t<tr onclick=\"Interviewer.openElement('/catalog', 'request=open-questionnaire")
+                    .concat("&indicated-questionnaire-id=")
+                    .concat(String.valueOf(immutablePair.getQObject().getId()))
+                    .concat("')\">\r\n\t\t\t<td>")
+                    .concat(immutablePair.getQObject().getTitle())
+                    .concat("</td>\r\n\t\t\t<td>")
+                    .concat(immutablePair.getGObject())
+                    .concat("</td>\r\n\t\t</tr>");
+        }
+        answerTable = answerTable.concat("\r\n\t</tbody>\r\n</table>");
         return answerTable;
     }
 
